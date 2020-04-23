9cc7b32e6d8821d5a16953fffab883140d9b7667
Megvii Engine Team <megengine@megvii.com>
2020-04-23 14:37:05 +0800

    refactor(dnn/arm): refactor arm_common pooling for add nchw44 pooling
    GitOrigin-RevId: 3980ceddeed8153aaf02488e0918a375809f77b8

--- dnn/src/fallback/pooling/opr_impl.h
+++ dnn/src/fallback/pooling/opr_impl.h
@@ -6,27 +6,28 @@
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT ARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * "AS IS" BASIS, WITHOUT ARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
  */
 #pragma once
+#include "megdnn/oprs/base.h"
 #include "src/naive/pooling/opr_impl.h"
 
 namespace megdnn {
 namespace fallback {
 
-class PoolingImpl: public naive::PoolingForwardImpl {
-    public:
-        using naive::PoolingForwardImpl::PoolingForwardImpl;
-        void exec(_megdnn_tensor_in src, _megdnn_tensor_out dst,
-                _megdnn_workspace workspace) override;
-    private:
-        void exec_w3x3_s1x1(_megdnn_tensor_in src, _megdnn_tensor_out dst);
-        void exec_w2x2_s2x2_int8(_megdnn_tensor_in src, _megdnn_tensor_out dst);
-        void exec_w2x2_s2x2_avg_int8(_megdnn_tensor_in src,
-                _megdnn_tensor_out dst);
-};
+class PoolingImpl : public naive::PoolingForwardImpl {
+public:
+    using naive::PoolingForwardImpl::PoolingForwardImpl;
+    void exec(_megdnn_tensor_in src, _megdnn_tensor_out dst,
+              _megdnn_workspace workspace) override;
 
-} // namespace fallback
-} // namespace megdnn
+private:
+    void exec_w3x3_s1x1(_megdnn_tensor_in src, _megdnn_tensor_out dst);
+    void exec_w2x2_s2x2_int8(_megdnn_tensor_in src, _megdnn_tensor_out dst);
+    void exec_w2x2_s2x2_avg_int8(_megdnn_tensor_in src, _megdnn_tensor_out dst);
+};
+}  // namespace fallback
+}  // namespace megdnn
 // vim: syntax=cpp.doxygen
 
