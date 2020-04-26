26738d99ebb37d8481bf77aaa598f0e496dfa870
Megvii Engine Team <megengine@megvii.com>
2020-04-26 18:26:26 +0800

    feat(mge/data): add objects365 dataset
    GitOrigin-RevId: 8eccebfc1de6a03b549f3dae1783fe17015cd723

--- python_module/megengine/data/dataset/vision/__init__.py
+++ python_module/megengine/data/dataset/vision/__init__.py
@@ -13,4 +13,5 @@ from .folder import ImageFolder
 from .imagenet import ImageNet
 from .meta_vision import VisionDataset
 from .mnist import MNIST
+from .objects365 import Objects365
 from .voc import PascalVOC
