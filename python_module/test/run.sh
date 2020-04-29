d3730036ea2091ae0989dbe9c9206dca3b947514
Megvii Engine Team <megengine@megvii.com>
2020-04-29 14:02:05 +0800

    test(mge/dist): add unit tests for megengine.distributed.utils
    GitOrigin-RevId: 32336c66f1cf68b818093148f1f356caa170f8cb

--- python_module/test/run.sh
+++ python_module/test/run.sh
@@ -1,14 +1,19 @@
 #!/bin/bash -e
 
+ignore_list="--ignore test/unit/module/test_pytorch.py \
+            --ignore test/pytorch_comparison \
+            --ignore test/unit/hub/test_hub.py \
+            --ignore test/unit/data \
+            --ignore test/integration/manual \
+            --ignore megengine/module/pytorch \
+            --ignore test/unit/module/test_external.py"
+test_dirs="megengine test"
+
 pushd $(dirname "${BASH_SOURCE[0]}")/.. >/dev/null
-    pytest -xv -m 'not internet' \
+    pytest -xv -m 'isolated_distributed' \
         --json-report --json-report-file=time_python_test.json \
-        --ignore test/unit/module/test_pytorch.py \
-        --ignore test/pytorch_comparison \
-        --ignore test/unit/hub/test_hub.py \
-        --ignore test/unit/data \
-        --ignore test/integration/manual \
-        --ignore megengine/module/pytorch \
-        --ignore test/unit/module/test_external.py \
-        megengine test
+        $ignore_list $test_dirs
+    pytest -xv -m 'not internet and not isolated_distributed' \
+        --json-report --json-report-file=time_python_test.json \
+        $ignore_list $test_dirs
 popd >/dev/null
