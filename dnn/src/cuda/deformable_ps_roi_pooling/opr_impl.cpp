6078187e32c299661c7574796dd6bb747466891c
Megvii Engine Team <megengine@megvii.com>
2020-07-03 10:45:54 +0800

    fix(dnn/cuda): fix indexing logic in psroi_pooling
    a variable relating to indexing was not computed correctly
    
    GitOrigin-RevId: 548c8f3f145c6d48212894fd6040331aff78a0b0

--- dnn/src/cuda/deformable_ps_roi_pooling/opr_impl.cpp
+++ dnn/src/cuda/deformable_ps_roi_pooling/opr_impl.cpp
@@ -32,7 +32,7 @@ void create_param(const DeformablePSROIPoolingBase* opr,
     p.sample_per_part = param.sample_per_part;
     p.trans_std = param.trans_std;
     p.scale = param.spatial_scale;
-    p.nr_cls = p.no_trans ? 1 : trans[0];
+    p.nr_cls = p.no_trans ? 1 : trans[1] / 2;
     p.nr_bbox = rois[0];
     p.IC = data[1];
     p.IH = data[2];
