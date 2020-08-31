c5e433b5684d8031f6764a7595906922c65d2ee7
Megvii Engine Team <megengine@megvii.com>
2020-08-31 14:12:29 +0800

    refactor(mge): cpp license
    GitOrigin-RevId: 2f67f84724f628c0477d468c3f741b0876a939ee

--- imperative/src/impl/op_trait.cpp
+++ imperative/src/impl/op_trait.cpp
@@ -1,10 +1,12 @@
 /**
- * \file src/core/include/megbrain/imperative.h
+ * \file imperative/src/impl/op_trait.cpp
+ * MegEngine is Licensed under the Apache License, Version 2.0 (the "License")
  *
- * This file is part of MegBrain, a deep learning framework developed by Megvii.
+ * Copyright (c) 2014-2020 Megvii Inc. All rights reserved.
  *
- * \copyright Copyright (c) 2014-2019 Megvii Inc. All rights reserved.
- *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT ARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  */
 
 #include <sstream>
--- imperative/src/impl/ops/opr_attr.cpp
+++ imperative/src/impl/ops/opr_attr.cpp
@@ -1,10 +1,12 @@
 /**
- * \file src/core/impl/imperative/physical_tensor.cpp
+ * \file imperative/src/impl/ops/opr_attr.cpp
+ * MegEngine is Licensed under the Apache License, Version 2.0 (the "License")
  *
- * This file is part of MegBrain, a deep learning framework developed by Megvii.
+ * Copyright (c) 2014-2020 Megvii Inc. All rights reserved.
  *
- * \copyright Copyright (c) 2014-2019 Megvii Inc. All rights reserved.
- *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT ARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  */
 
 #include "megbrain/imperative/ops/opr_attr.h"
