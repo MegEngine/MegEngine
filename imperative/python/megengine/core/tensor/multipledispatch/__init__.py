099ffeac1204f008a74f388726f263c9a8f25c25
Megvii Engine Team <megengine@megvii.com>
2020-09-14 13:54:30 +0800

    chore(mge): typo and reword
    GitOrigin-RevId: 3848da482464e9058e47597b7b552ed173877c12

--- imperative/python/megengine/core/tensor/multipledispatch/__init__.py
+++ imperative/python/megengine/core/tensor/multipledispatch/__init__.py
@@ -46,7 +46,8 @@
 # Commit: 9e3c87d0cee57972fd5cc33fe5cacde77c781834
 # Authors: Matthew Rocklin et al.
 #
-# Refer to ACKNOWLEDGEMENT for copyright and liscense information
+# The original LICENSE file is included in the ACKNOWLEDGEMENT file under
+# MegEngine root directory.
 
 from .core import dispatch
 from .dispatcher import Dispatcher
