f63bca032c3c05ae86825bfef3a35b0725272a48
Megvii Engine Team <megengine@megvii.com>
2020-09-22 14:43:36 +0800

    test(mge/dtype): add quant and lowbit dtype test
    GitOrigin-RevId: 97ca1a393a7dc21e2c65d2cb9c38f7cb6b8581e0

---
+++ imperative/python/test/unit/core/test_dtype_bfloat16.py
@@ -0,0 +1,63 @@
+# MegEngine is Licensed under the Apache License, Version 2.0 (the "License")
+#
+# Copyright (c) 2014-2020 Megvii Inc. All rights reserved.
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT ARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+import pickle
+
+import numpy as np
+
+from megengine.core.tensor.dtype import bfloat16
+from megengine.core.tensor.raw_tensor import as_raw_tensor
+
+
+def test_define():
+    np.testing.assert_allclose(
+        np.array([0.5, 0.13425, 3.4687, -1.34976, -9.34673, 0.0], dtype=bfloat16),
+        np.array([0.5, 0.133789, 3.46875, -1.351562, -9.375, 0.0], dtype=np.float32),
+        atol=1e-6,
+    )
+
+
+def test_cast():
+    dtypes = [np.int8, np.int16, np.int32, np.float32, np.float64]
+    fp32_values = [0.34985, 10.943, -0.5, -19.3, 21.49673]
+    bf16_values = [0.349609, 10.9375, -0.5, -19.25, 21.5]
+    int_values = [34, 10, -5, -19, 21]
+    for dtype in dtypes:
+        np.testing.assert_allclose(
+            np.array(fp32_values, dtype=bfloat16).astype(dtype),
+            np.array(bf16_values, dtype=dtype),
+            atol=1e-6,
+        )
+        np.testing.assert_allclose(
+            np.array(int_values, dtype=dtype),
+            np.array(int_values, dtype=bfloat16).astype(dtype),
+            atol=1e-6,
+        )
+
+
+def test_shared_nd():
+    data = np.array([-3.4, 1.394683, 2.323497, -7.439948, -5.2397], dtype=bfloat16)
+    snd = as_raw_tensor(data, dtype=bfloat16, device="xpux")
+    assert snd.numpy().dtype == bfloat16
+    np.testing.assert_allclose(
+        snd.numpy(), [-3.40625, 1.398438, 2.328125, -7.4375, -5.25], atol=1e-6
+    )
+
+    data = np.array([-9.34964, -8.342, 9.4385, 0.18746, 1.48], dtype=bfloat16)
+    snd = as_raw_tensor(data, dtype=bfloat16, device="xpux")
+    np.testing.assert_allclose(
+        snd.numpy(), [-9.375, -8.3125, 9.4375, 0.1875, 1.476562], atol=1e-6
+    )
+
+
+def test_pickle():
+    x = np.ascontiguousarray(np.random.rand(8192), dtype=bfloat16)
+    pkl = pickle.dumps(x, pickle.HIGHEST_PROTOCOL)
+    y = pickle.loads(pkl)
+    assert x.dtype is y.dtype
+    np.testing.assert_allclose(x.astype(np.float32), y.astype(np.float32), atol=1e-6)
---
+++ imperative/python/test/unit/core/test_dtype_intbx.py
@@ -0,0 +1,100 @@
+# MegEngine is Licensed under the Apache License, Version 2.0 (the "License")
+#
+# Copyright (c) 2014-2020 Megvii Inc. All rights reserved.
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT ARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+import pickle
+
+import numpy as np
+import pytest
+
+from megengine.core.tensor.dtype import intb1, intb2, intb4
+from megengine.core.tensor.raw_tensor import as_raw_tensor
+
+
+def bit_define_test(bit, low_bit_type):
+    max_value = (1 << bit) - 1
+    min_value = 1 - (1 << bit)
+
+    a = np.array([i for i in range(min_value, max_value + 2, 2)], dtype=low_bit_type)
+
+    for i in range(max_value + 1):
+        np.testing.assert_equal(a[i], i * 2 - max_value)
+        np.testing.assert_equal(str(a[i]), str(i * 2 - max_value))
+
+    with pytest.raises(ValueError):
+        np.arange(min_value, max_value, dtype=low_bit_type)
+
+    with pytest.raises(ValueError):
+        np.arange(min_value - 2, max_value + 4, 2, dtype=low_bit_type)
+
+    np.testing.assert_allclose(
+        np.arange(min_value, 12, 2, dtype=low_bit_type),
+        (np.arange((13 - min_value) // 2, dtype=np.int8) % (max_value + 1)) * 2
+        - max_value,
+    )
+
+    np.testing.assert_allclose(
+        np.arange(max_value, max_value - 20, -2, dtype=low_bit_type),
+        (np.arange(max_value, max_value - 10, -1, dtype=np.int8) % (max_value + 1)) * 2
+        - max_value,
+    )
+
+
+def test_define():
+    bit_define_test(1, intb1)
+    bit_define_test(2, intb2)
+    bit_define_test(4, intb4)
+
+
+def _bit_cast_test(bit, low_bit_type):
+    dtypes = [np.int8, np.int16, np.int32, np.float32, np.float64]
+
+    max_value = (1 << bit) - 1
+    min_value = 1 - (1 << bit)
+    for dtype in dtypes:
+        np.testing.assert_allclose(
+            np.arange(min_value, max_value + 2, 2, dtype=low_bit_type).astype(dtype),
+            np.arange(min_value, max_value + 2, 2, dtype=dtype),
+        )
+
+    with pytest.raises(ValueError):
+        np.array([2, 1, -1], dtype=int).astype(low_bit_type)
+    with pytest.raises(ValueError):
+        np.array([min_value - 2, 1, max_value + 2], dtype=int).astype(low_bit_type)
+
+
+def test_cast():
+    _bit_cast_test(1, intb1)
+    _bit_cast_test(2, intb2)
+    _bit_cast_test(4, intb4)
+
+
+def _shared_nd_test(bit, low_bit_type):
+    max_value = (1 << bit) - 1
+    min_value = 1 - (1 << bit)
+
+    data = np.arange(min_value, max_value + 2, 2, dtype=low_bit_type)
+    snd = as_raw_tensor(data, dtype=low_bit_type, device="xpux")
+    np.testing.assert_allclose(snd.numpy(), range(min_value, max_value + 2, 2))
+
+    data = np.arange(min_value, max_value + 2, 4, dtype=low_bit_type)
+    snd = as_raw_tensor(data, dtype=low_bit_type, device="xpux")
+    np.testing.assert_allclose(snd.numpy(), range(min_value, max_value + 2, 4))
+
+
+def test_shared_nd():
+    _shared_nd_test(1, intb1)
+    _shared_nd_test(2, intb2)
+    _shared_nd_test(4, intb4)
+
+
+def test_pickle():
+    x = np.ascontiguousarray(np.random.randint(2, size=8192) * 2 - 1, dtype=intb1)
+    pkl = pickle.dumps(x, pickle.HIGHEST_PROTOCOL)
+    y = pickle.loads(pkl)
+    assert x.dtype is y.dtype
+    np.testing.assert_allclose(x.astype(np.float32), y.astype(np.float32))
