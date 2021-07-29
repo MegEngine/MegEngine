bc2b1690c968cb5aed21b9e65c3ce87e959fa74c
Megvii Engine Team <megengine@megvii.com>
2021-07-29 18:17:47 +0800

    ci(thirdparty): add third_party cache
    GitOrigin-RevId: d54681f0c099a60e816a085ce77084b8957b0592

--- third_party/prepare.sh
+++ third_party/prepare.sh
@@ -15,39 +15,50 @@ if [ "$(printf '%s\n' "$requiredGitVersion" "$currentGitVersion" | ${SORT} -V | 
     echo "Please update your Git version. (foud version $currentGitVersion, required version >= $requiredGitVersion)"
     exit -1
 fi
-git submodule sync
-git submodule update -f --init midout
-git submodule update -f --init intel-mkl-dnn
-git submodule update -f --init Halide
-git submodule update -f --init protobuf
-git submodule update -f --init gtest
-git submodule update -f --init flatbuffers
-git submodule update -f --init cutlass
-git submodule update -f --init Json
-git submodule update -f --init pybind11
-git submodule update -f --init llvm-project
-git submodule update -f --init range-v3
-git submodule update -f --init libzmq
-git submodule update -f --init cppzmq
-git submodule update -f --init OpenBLAS
-git submodule update -f --init cpuinfo
 
-name=`git config --get user.name || true`
-if [ -z "$name" ]; then
-    name="default"
-fi
-email=`git config --get user.email || true`
-if [ -z "$email" ]; then
-    email="default"
-fi
+function update_cpuinfo() {
+    git submodule update -f --init cpuinfo
 
-source ./apply-patches.sh
-apply_cpuinfo_patches ${name} ${email}
+    name=`git config --get user.name || true`
+    if [ -z "$name" ]; then
+        name="default"
+    fi
+    email=`git config --get user.email || true`
+    if [ -z "$email" ]; then
+        email="default"
+    fi
 
-git submodule update -f --init MegRay
-pushd MegRay/third_party >/dev/null
+    source ./apply-patches.sh
+    apply_cpuinfo_patches ${name} ${email}
+}
+
+function git_submodule_update() {
     git submodule sync
-    git submodule update -f --init nccl
-    git submodule update -f --init gdrcopy
-    git submodule update -f --init ucx
-popd >/dev/null
+    git submodule update -f --init midout
+    git submodule update -f --init intel-mkl-dnn
+    git submodule update -f --init Halide
+    git submodule update -f --init protobuf
+    git submodule update -f --init gtest
+    git submodule update -f --init flatbuffers
+    git submodule update -f --init cutlass
+    git submodule update -f --init Json
+    git submodule update -f --init pybind11
+    git submodule update -f --init llvm-project
+    git submodule update -f --init range-v3
+    git submodule update -f --init libzmq
+    git submodule update -f --init cppzmq
+    git submodule update -f --init OpenBLAS
+    update_cpuinfo
+
+    git submodule update -f --init MegRay
+    pushd MegRay/third_party >/dev/null
+        git submodule sync
+        git submodule update -f --init nccl
+        git submodule update -f --init gdrcopy
+        git submodule update -f --init ucx
+    popd >/dev/null
+}
+
+if [[ -z "${ALREADY_UPDATE_SUBMODULES}" ]]; then
+    git_submodule_update
+fi
