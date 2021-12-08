744efe72a71f565f9cde046b2001ec1e7d9828e8
Megvii Engine Team <megengine@megvii.com>
2021-12-08 18:38:37 +0800

    fix(mge/utils): fix module_status import error
    GitOrigin-RevId: 1e04186450ecae85f0af1074ccdc2f4993a73f35

--- imperative/python/megengine/utils/module_stats.py
+++ imperative/python/megengine/utils/module_stats.py
@@ -5,7 +5,8 @@
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT ARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-from collections import Iterable, namedtuple
+import collections
+from collections import namedtuple
 from functools import partial
 from typing import Iterable
 
