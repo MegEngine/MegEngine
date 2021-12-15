608c84d887d80e2feb5701a139204aeebcf9bec6
Megvii Engine Team <megengine@megvii.com>
2021-12-15 19:50:09 +0800

    feat(imperative): add more tools for megengine
    GitOrigin-RevId: 175d0f7f57a668dee196a34a1e153d828575cd28

--- imperative/python/megengine/tools/README.md
+++ imperative/python/megengine/tools/README.md
@@ -1,8 +1,166 @@
 # MegEngine Tools
 
-This directory contains executable python files.
-Use these files in the following way (replace `xxx` to specific file name, like `network_visualize`):
+MegEngine 相关的工具汇总。使用方法如下(可将 `xxx` 替换成任一脚本文件，如 `network_visualize`):
 
-```
+```bash
 python -m megengine.tools.xxx
 ```
+
+工具列表:
+
+### accuracy_shake_var_tree
+
+将精度抖动分析结果构造成树结构，方便锁定引起抖动的根节点，以及查找依赖关系。
+
+输入: compare_binary_iodump 的输出存入到的一个文件
+
+输出: 第一个出现结果不一致的输出结点
+
+执行命令: accuracy_shake_var_tree 中定义了一些函数组件，可按需集成到实际代码中。下面有一个测试代码:
+
+```python
+import megengine.tools.accuracy_shake_var_tree as st
+
+r = st.parse('diff.txt')
+for key, value in r.items():
+    n = st.varNode.get_varNode(key)
+    n.show_src_info()
+    print("reference nodes:")
+    for i in n.get_reference_list():
+        print(i.id)
+```
+
+### benchmark_op
+
+逐个运行 functional op(并不是所有的 functional op)，对比 MegEngine 与 PyTorch 的性能，通过量化结果来指导如何进行下一步的优化。
+
+输入: 无
+
+输出: 打印一个列表，对比在小输入和大输入的情况下 MegEngine 和 Pytorch 执行一些 functional op 的速度对比
+
+执行命令: `python3 -m megengine.tools.benchmark_op`
+
+### compare_binary_iodump
+
+分析同一模型在不同平台下给定相同输入之后的输出是否完全一致。
+
+输入: 两个目录(假设分别为 expect/ 和 actual/)，分别存有不同平台下运行的 tensor 结果
+
+输出: 打印所有的输出 tensor 信息，如果某个 tensor 在两个平台上的值不一致，那么会打印出第一个不一致的值
+
+执行命令: `python3 -m megengine.tools.compare_binary_iodump expect/ actual/`
+
+### cpu_evaluation_tools
+
+分析多个模型在目标芯片上的运行性能
+
+输入：MegEngine 模型文件
+
+输出：根据不同模型的加权，输出芯片性能分数
+
+执行命令：python3 ./cpu_evaluation_tools.py --load_and_run_file ./load_and_run --models_dir ./cpu_models/
+
+### draw_graph
+
+用来查看静态图的 op 序列，有助于理解 MegEngine 的静态图在动态图的基础上做了哪些优化。
+
+输入: `megengine.core.tensor.megbrain_graph.Graph._to_json` 得出的静态图描述文件，为 json 格式
+
+输出: 一个 dot 文件，可通过 dot 命令绘制出图片
+
+执行命令:
+
+```bash
+python3 -m megengine.tools.draw_graph -i dump.json -o dump.dot
+dot -Tpng dump.dot -o dump.png
+```
+
+### dump_with_testcase_mge
+
+将待测数据提前注入模型文件，并在本地运行得到期望结果，可与实际运行的结果进行比对以检查是否出错。
+
+输入: 一个 MegEngine 模型文件，可选一些 npy 文件作为模型输入(也可以随机生成输入，如下面的命令示例)
+
+输出: 一个带输入的 MegEngine 模型文件
+
+执行命令: `python3 -m megengine.tools.dump_with_testcase_mge model.mge -d "#rand(0,255,14,2)"`
+
+### graph_info_analyze
+
+将图和内存信息的 json 文件的文件夹 logs 转换为 TensorBoard 的输入文件夹 logs_p。以便 TensorBoard 对图结构以及内存信息进行可视化。
+
+输入: 图和内存信息的 json 文件的文件夹
+
+输出: TensorBoard 的输入文件夹
+
+执行命令: `python3 -m megengine.tools.graph_info_analyze -i logs -o logs_p`
+
+### load_network_and_run
+
+python 版本的 load_and_run。
+
+输入: MegEngine 的模型文件，可选一些 npy 文件作为模型输入
+
+输出: 模型执行并打印一些测速信息
+
+执行命令: `python3 -m megengine.tools.load_network_and_run model.mge --iter 10`
+
+### network_visualize
+
+1. 分析给定的 MegEngine 模型中参数量信息，包括 shape、dtype、mean、std 以及 size 占比等。
+2. 分析给定的 MegEngine 模型中算子 FLOPs 计算量以及占比，还有算子的 inputs/outputs shape、感受野、stride 等。
+
+输入: MegEngine 的模型文件
+
+输出: 模型中的参数量信息或计算量信息
+
+执行命令:
+
+```bash
+# 分析参数量
+python3 -m megengine.tools.network_visualize model.mge --cal_params --logging_to_stdout
+
+# 分析计算量
+python3 -m megengine.tools.network_visualize model.mge --cal_flops --logging_to_stdout
+```
+
+### profile_analyze
+
+对于 load_and_run --profile 运行模型生成的 profile.json 文件或者 trace 模式下开启 profiling 功能并通过 trace.get_profile() 得到的 json 文件进行分析，得到静态图中算子的时间和显存占比等信息，以表格形式呈现。
+
+输入: load_and_run 生成的 profile 文件
+
+输出: 一个按照参数在输入文件中筛选得出的数据表格
+
+执行命令:
+
+```bash
+# 生成供分析的 json 文件
+python3 -m megengine.tools.load_network_and_run model.mge --warm-up --iter 10 --profile profile.json
+
+#分析耗时前 3 的单个算子
+python3 -m megengine.tools.profile_analyze profile.json -t 3
+
+#筛选用时超过 10us 的 conv 按 flops 排序
+python3 -m megengine.tools.profile_analyze profile.json -t 3 --order-by +flops --min-time 1e-5 --type ConvolutionForward
+```
+
+### profiler
+
+对给定的训练程序，记录训练过程并以通用格式存储，可在浏览器上可视化。
+
+输入: 需要一个 MegEngine 的训练程序(称之为 train.py，其中包含一个典型的 MegEngine 训练过程)
+
+输出: 一些记录 profile 过程的 json 文件，默认在 profile 子目录下，可用 https://ui.perfetto.dev/ 进行加载并且可视化
+
+执行命令: `python3 -m megengine.tools.profiler train.py`
+
+### svg_viewer
+
+查看 MegEngine 生成的显存占用图，可以帮助用户了解显存使用情况.
+
+输入: 显存占用的 svg 图片
+
+输出: 网页展示的可视化
+
+执行命令: `python3 -m megengine.tools.svg_viewer`
---
+++ imperative/python/megengine/tools/benchmark_op.py
@@ -0,0 +1,476 @@
+# -*- coding: utf-8 -*-
+# MegEngine is Licensed under the Apache License, Version 2.0 (the "License")
+#
+# Copyright (c) 2014-2021 Megvii Inc. All rights reserved.
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT ARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+import time
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as TF
+from tabulate import tabulate
+
+import megengine as mge
+import megengine.functional as MF
+import megengine.module as MM
+
+module_cache = {
+    "conv2d": (MM.Conv2d(32, 32, 3, 1, 0), nn.Conv2d(32, 32, 3, 1, 0).cuda()),
+    "dw_conv2d": (
+        MM.Conv2d(32, 32, 3, 1, 0, groups=32),
+        nn.Conv2d(32, 32, 3, 1, 0, groups=32).cuda(),
+    ),
+    "conv3d": (MM.Conv3d(32, 32, 3, 1, 0), nn.Conv3d(32, 32, 3, 1, 0).cuda()),
+    "ConvTranspose2d": (
+        MM.ConvTranspose2d(32, 32, 3, 1, 0),
+        nn.ConvTranspose2d(32, 32, 3, 1, 0).cuda(),
+    ),
+    "BatchNorm2d": (MM.BatchNorm2d(64), nn.BatchNorm2d(64).cuda()),
+    "Linear": (MM.Linear(1000, 1000), nn.Linear(1000, 1000).cuda()),
+}
+
+test_cases = [
+    # (mge op, torch op, small inps, large inps, unpack_inps, rep)
+    (
+        "adaptive_avg_pool2d",
+        lambda x: MF.adaptive_avg_pool2d(x, (7, 7)),
+        lambda x: TF.adaptive_avg_pool2d(x, (7, 7)),
+        [(2, 32, 16, 16)],
+        [(64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    (
+        "adaptive_max_pool2d",
+        lambda x: MF.adaptive_max_pool2d(x, (7, 7)),
+        lambda x: TF.adaptive_max_pool2d(x, (7, 7)),
+        [(2, 32, 16, 16)],
+        [(64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    ("argsort", MF.argsort, torch.argsort, [(1000,)], [(1000, 1000),], True, 1000),
+    (
+        "avg_pool2d",
+        lambda x: MF.avg_pool2d(x, 2),
+        lambda x: TF.avg_pool2d(x, 2),
+        [(2, 32, 16, 16)],
+        [(64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    (
+        "broadcast",
+        lambda x: MF.broadcast_to(x, (5,) + x.shape),
+        lambda x: torch.broadcast_to(x, (5,) + x.shape),
+        [(100, 100)],
+        [(64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    (
+        "batchedmatmul",
+        MF.matmul,
+        torch.matmul,
+        [(8, 64, 32), (8, 32, 64)],
+        [(8, 2048, 512), (8, 512, 2048)],
+        True,
+        1000,
+    ),
+    (
+        "batchnrom2d",
+        lambda x: module_cache["BatchNorm2d"][0](x),
+        lambda x: module_cache["BatchNorm2d"][1](x),
+        [(2, 64, 16, 16)],
+        [(64, 64, 128, 128)],
+        True,
+        1000,
+    ),
+    (
+        "concat",
+        MF.concat,
+        torch.cat,
+        [(20, 100), (50, 100), (30, 100)],
+        [(64, 512, 16, 16), (64, 512, 16, 16), (64, 512, 16, 16)],
+        False,
+        1000,
+    ),
+    (
+        "conv2d",
+        lambda x: module_cache["conv2d"][0](x),
+        lambda x: module_cache["conv2d"][1](x),
+        [(2, 32, 16, 16)],
+        [(32, 32, 128, 128)],
+        True,
+        1000,
+    ),
+    (
+        "conv3d",
+        lambda x: module_cache["conv3d"][0](x),
+        lambda x: module_cache["conv3d"][1](x),
+        [(2, 32, 8, 8, 8)],
+        [(32, 32, 16, 16, 16)],
+        True,
+        1000,
+    ),
+    (
+        "convTranspose2d",
+        lambda x: module_cache["ConvTranspose2d"][0](x),
+        lambda x: module_cache["ConvTranspose2d"][1](x),
+        [(2, 32, 16, 16)],
+        [(32, 32, 128, 128)],
+        True,
+        1000,
+    ),
+    (
+        "dropout",
+        lambda x: MF.dropout(x, 0.5),
+        TF.dropout,
+        [(100, 100)],
+        [(64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    (
+        "dw_conv2d",
+        lambda x: module_cache["dw_conv2d"][0](x),
+        lambda x: module_cache["dw_conv2d"][1](x),
+        [(2, 32, 16, 16)],
+        [(32, 32, 128, 128)],
+        True,
+        1000,
+    ),
+    (
+        "elemwise.unary",
+        MF.log,
+        torch.log,
+        [(100, 100)],
+        [(64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    (
+        "elemwise.binary",
+        MF.add,
+        torch.add,
+        [(100, 100), (100, 100)],
+        [(64, 512, 16, 16), (64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    (
+        "expand_dims",
+        lambda x: MF.expand_dims(x, 0),
+        lambda x: torch.unsqueeze(x, 0),
+        [(100, 100)],
+        [(64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    ("gelu", MF.gelu, TF.gelu, [(100, 100)], [(64, 512, 16, 16)], True, 1000),
+    ("hswish", MF.hswish, TF.hardswish, [(100, 100)], [(64, 512, 16, 16)], True, 1000),
+    (
+        "hsigmoid",
+        MF.hsigmoid,
+        TF.hardsigmoid,
+        [(100, 100)],
+        [(64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    ("isinf", MF.isinf, torch.isinf, [(100, 100)], [(64, 512, 16, 16)], True, 1000),
+    (
+        "indeixngMultiAxisVec",
+        lambda x: x[[1, 3, 5], [1, 3, 5], [1, 3, 5], [1, 3, 5]],
+        lambda x: x[[1, 3, 5], [1, 3, 5], [1, 3, 5], [1, 3, 5]],
+        [(10, 10, 10, 10)],
+        [(64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    (
+        "logsigmoid",
+        MF.logsigmoid,
+        TF.logsigmoid,
+        [(100, 100)],
+        [(64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    (
+        "leaky_relu",
+        lambda x: MF.leaky_relu(x, 0.5),
+        lambda x: TF.leaky_relu(x, 0.5),
+        [(100, 100)],
+        [(64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    (
+        "linear",
+        lambda x: module_cache["Linear"][0](x),
+        lambda x: module_cache["Linear"][1](x),
+        [(10, 1000)],
+        [(64, 128, 1000)],
+        True,
+        1000,
+    ),
+    ("matinv", MF.matinv, torch.inverse, [(10, 10)], [(30, 30)], True, 1000),
+    (
+        "matmul",
+        MF.matmul,
+        torch.matmul,
+        [(64, 32), (32, 64)],
+        [(2048, 1024), (1024, 2048)],
+        True,
+        1000,
+    ),
+    (
+        "max_pool2d",
+        lambda x: MF.max_pool2d(x, 2),
+        lambda x: TF.max_pool2d(x, 2),
+        [(2, 32, 16, 16)],
+        [(64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    (
+        "normal",
+        lambda x: mge.random.normal(0, 1, x.shape),
+        lambda x: torch.randn(x.shape, device="cuda"),
+        [(100, 100)],
+        [(64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    (
+        "prelu",
+        MF.prelu,
+        TF.prelu,
+        [(100, 100), (1,)],
+        [(64, 512, 16, 16), (1,)],
+        True,
+        1000,
+    ),
+    (
+        "reduce.max",
+        lambda x: MF.max(x, 0),
+        lambda x: torch.max(x, 0),
+        [(100, 100)],
+        [(64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    (
+        "reduce.mean",
+        lambda x: MF.mean(x, 0),
+        lambda x: torch.mean(x, 0),
+        [(100, 100)],
+        [(64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    (
+        "reduce.mean",
+        lambda x: MF.mean(x, 0),
+        lambda x: torch.mean(x, 0),
+        [(100, 100)],
+        [(64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    ("relu", MF.relu, TF.relu, [(100, 100)], [(64, 512, 16, 16)], True, 1000),
+    ("relu6", MF.relu6, TF.relu6, [(100, 100)], [(64, 512, 16, 16)], True, 1000),
+    (
+        "repeat",
+        lambda x: MF.repeat(x, 5),
+        lambda x: torch.repeat_interleave(x, 5),
+        [(100, 100)],
+        [(64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    ("silu", MF.silu, TF.silu, [(100, 100)], [(64, 512, 16, 16)], True, 1000),
+    (
+        "split",
+        lambda x: MF.split(x, 5),
+        lambda x: torch.split(x, 5),
+        [(100, 100)],
+        [(64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    ("sigmoid", MF.sigmoid, TF.sigmoid, [(100, 100)], [(64, 512, 16, 16)], True, 1000),
+    (
+        "softmax",
+        lambda x: MF.softmax(x, axis=1),
+        lambda x: TF.softmax(x, dim=1),
+        [(100, 100)],
+        [(64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    (
+        "softplus",
+        MF.softplus,
+        TF.softplus,
+        [(100, 100)],
+        [(64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    (
+        "squeeze",
+        lambda x: MF.squeeze(x, 0),
+        lambda x: torch.squeeze(x, 0),
+        [(1, 100, 100)],
+        [(1, 64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    (
+        "stack",
+        MF.stack,
+        torch.stack,
+        [(100, 100), (100, 100)],
+        [(64, 512, 16, 16), (64, 512, 16, 16)],
+        False,
+        10000,
+    ),
+    (
+        "subtensor",
+        lambda x: x[0:20, 10:60],
+        lambda x: x[0:20, 10:60],
+        [(100, 100)],
+        [(64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    (
+        "topk",
+        lambda x: MF.topk(x, 10),
+        lambda x: torch.topk(x, 10),
+        [(100, 100)],
+        [(1000, 1000)],
+        True,
+        1000,
+    ),
+    (
+        "tile",
+        lambda x: MF.tile(x, (2,) * len(x.shape)),
+        lambda x: torch.tile(x, (2,) * len(x.shape)),
+        [(100, 100)],
+        [(64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    (
+        "transpose",
+        lambda x: MF.transpose(x, list(range(len(x.shape)))[::-1]),
+        lambda x: torch.permute(x, list(range(len(x.shape)))[::-1]),
+        [(100, 100)],
+        [(64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    (
+        "where",
+        lambda x: MF.where(x > 0.5, x, x),
+        lambda x: torch.where(x > 0.5, x, x),
+        [(100, 100)],
+        [(64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+    (
+        "uniform",
+        lambda x: mge.random.uniform(0, 1, x.shape),
+        lambda x: torch.rand(x.shape, device="cuda"),
+        [(100, 100)],
+        [(64, 512, 16, 16)],
+        True,
+        1000,
+    ),
+]
+
+
+def perf_func(func, inps, reps, unpack_inps, is_mge):
+    if is_mge:
+        mge._full_sync()
+        tik = time.time()
+        for _ in range(reps):
+            if unpack_inps:
+                out = func(*inps)
+            else:
+                out = func(inps)
+        mge._full_sync()
+    else:
+        torch.cuda.synchronize()
+        with torch.no_grad():
+            tik = time.time()
+            for _ in range(reps):
+                if unpack_inps:
+                    out = func(*inps)
+                else:
+                    out = func(inps)
+        torch.cuda.synchronize()
+    return time.time() - tik
+
+
+def get_avg_time(func, inps, reps, unpack_inps, is_mge):
+    # warm up
+    for _ in range(2):
+        t = perf_func(func, inps, reps, unpack_inps, is_mge)
+
+    times = []
+    for _ in range(5):
+        t = perf_func(func, inps, reps, unpack_inps, is_mge)
+        times.append(t)
+    return np.mean(times)
+
+
+def get_perf_results(mge_func, torch_func, shapes, unpack_inps, reps):
+    inps = [np.random.randn(*shape) for shape in shapes]
+
+    inps_mge = [mge.tensor(inp, dtype="float32") for inp in inps]
+    avg_time_mge = get_avg_time(mge_func, inps_mge, reps, unpack_inps, True)
+
+    inps_torch = [torch.Tensor(inp).type(torch.float).cuda() for inp in inps]
+    avg_time_torch = get_avg_time(torch_func, inps_torch, reps, unpack_inps, False)
+
+    return avg_time_mge, avg_time_torch
+
+
+if __name__ == "__main__":
+    header = [
+        "opr_name",
+        "time(mge/pytorch; small input)",
+        "time(mge/pytorch; large input)",
+    ]
+    table = []
+    for case in test_cases:
+        assert len(case) == 7
+        name, mge_func, torch_func, small_shapes, large_shapes, unpack_inps, reps = case
+        data = []
+        data.append(name)
+        print("========== op: {}".format(name))
+
+        avg_time_mge, avg_time_torch = get_perf_results(
+            mge_func, torch_func, small_shapes, unpack_inps, reps
+        )
+        print("mge time: {}".format(avg_time_mge))
+        print("torch time: {}".format(avg_time_torch))
+        data.append("{:.2f}".format(avg_time_mge / avg_time_torch))
+
+        avg_time_mge, avg_time_torch = get_perf_results(
+            mge_func, torch_func, large_shapes, unpack_inps, reps
+        )
+        print("mge time: {}".format(avg_time_mge))
+        print("torch time: {}".format(avg_time_torch))
+        data.append("{:.2f}".format(avg_time_mge / avg_time_torch))
+        table.append(data)
+    print(tabulate(table, header, tablefmt="github"))
