3251f5011474bff0a9d822f61de938aeafa79be3
Megvii Engine Team <megengine@megvii.com>
2022-01-13 17:28:37 +0800

    fix(mgb/cuda-stub): add libcuda-wrap_11.4.h to fit the CUDA11.4 toolchain
    GitOrigin-RevId: efa38f00d15518ed3622eab085a210c5018d782d

--- dnn/cuda-stub/src/libcuda.cpp
+++ dnn/cuda-stub/src/libcuda.cpp
@@ -56,6 +56,8 @@ CUresult on_init_failed(int func_idx) {
 #include "./libcuda-wrap_11.1.h"
 #elif CUDA_VERSION == 11020
 #include "./libcuda-wrap_11.2.h"
+#elif CUDA_VERSION == 11040
+#include "./libcuda-wrap_11.4.h"
 #else
 #error "cuda stub not support this cuda version, you can close cuda stub to passby"
 #endif
