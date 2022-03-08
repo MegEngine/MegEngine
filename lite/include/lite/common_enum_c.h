04475744466000519a92999982048856c2daf19d
Megvii Engine Team <megengine@megvii.com>
2022-03-08 13:22:42 +0800

    feat(opencl): add OpenCL cache compat level api
    GitOrigin-RevId: e7561e6879bcdbfee52d48387c8ec2a7f21a91d6

--- lite/include/lite/common_enum_c.h
+++ lite/include/lite/common_enum_c.h
@@ -105,5 +105,23 @@ typedef enum {
     LITE_ALGO_OPTIMIZED = 1 << 3,
 } LiteAlgoSelectStrategy;
 
+/*!
+ * \brief enum for cache compat level, for example: adreno 630 cache may be apply to
+ * adreno 640, if you do not want search cache for adreno 640, just config SERIES_COMPAT
+ * or VENDOR_COMPAT, adreno 506 cache may be apply to adreno 630, if you do not want
+ * search cache for adreno 630, just config VENDOR_COMPAT
+ * WARN: this config just let program_cache_io try `use a old cache` for `device compile
+ * the cache` do not means MegEngine will insure the compile will be ok! it`s a device
+ * CL driver behavior, if compile failed!, MegEngine will try build from source, What`s
+ * more, even though compile from binary success, this cross-use-cache may affect
+ * performance, VENDOR_COMPAT will contain SERIES_COMPAT
+ */
+typedef enum {
+    LITE_NOT_COMPAT = 0,     //! default not compat for series and vendor
+    LITE_SERIES_COMPAT = 1,  //! for scene adreno 640 use adreno 630 cache
+    LITE_VENDOR_COMPAT = 2,  //! for scene adreno 630 use adreno 506 cache
+    LITE_CACHE_COMPAT_LEVEL_CNT = 3
+} LiteOpenCLCacheCompatLevel;
+
 #endif
 // vim: syntax=cpp.doxygen foldmethod=marker foldmarker=f{{{,f}}}
