0948f5870df466d68d905c705211daa9695572e9
Megvii Engine Team <megengine@megvii.com>
2022-03-23 15:56:39 +0800

    ci(mge): download mkl from ftp
    GitOrigin-RevId: c67b808121ac15aafa5c040e329c046ccb2092f6

--- third_party/install-mkl.sh
+++ third_party/install-mkl.sh
@@ -7,12 +7,14 @@ echo "this script only for linux/macos/windows-unix-like-env(MSYS etc) prepare M
 echo "if you build windows for native at cmd.exe, powershell env or Visual Studio GUI,"
 echo  "u need download MKL package and untar manually"
 echo "refs: https://software.intel.com/content/www/us/en/develop/tools/math-kernel-library/choose-download/windows.html"
-MKL_VERSION=2019.5
-MKL_PATCH=281
-CONDA_BASE_URL=https://anaconda.org/intel
+
 OS=$(uname -s)
-FILE_PREFIX=null
 TAR=tar
+
+if [[ -z ${MKL_VERSION} ]];then
+    MKL_VERSION=2019.5
+    MKL_PATCH=281
+fi
 if [ $OS = "Darwin" ];then
     FILE_PREFIX=osx
 elif [ $OS = "Linux" ];then
@@ -31,24 +33,26 @@ echo "config FILE_PREFIX to: $FILE_PREFIX"
 
 rm -rf mkl
 
-if [[ -z ${ALREADY_INSTALL_MKL} ]]
-then
-    echo "init mkl from software.intel.com"
-    for platform in 32 64
-    do
-        if [ $OS = "Darwin" ]&&[ $platform = 32 ];then
-            echo "strip 32 bit file for Darwin"
-            continue
-        fi
-        mkdir -p mkl/x86_${platform}
-        for package in "mkl-include" "mkl-static"
-        do
-            DOWNLOAD_FILE=${package}-${MKL_VERSION}-intel_${MKL_PATCH}.tar.bz2
-            echo "Installing ${DOWNLOAD_FILE} for x86_${platform}..."
-            URL=${CONDA_BASE_URL}/${package}/${MKL_VERSION}/download/$FILE_PREFIX-${platform}/${DOWNLOAD_FILE}
-            echo "try download mkl package from: ${URL}"
-            wget -q --show-progress "${URL}" -O mkl/x86_${platform}/${DOWNLOAD_FILE}
-            $TAR xvj -C mkl/x86_${platform} -f mkl/x86_${platform}/${DOWNLOAD_FILE}
-        done
-    done
+if [[ -z ${FTP_URL_PREFIX} ]];then
+    DOWNLOAD_FILE='${package}-${MKL_VERSION}-intel_${MKL_PATCH}.tar.bz2'
+    URL='https://anaconda.org/intel/${package}/${MKL_VERSION}/download/$FILE_PREFIX-${platform}/'${DOWNLOAD_FILE}
+else
+    DOWNLOAD_FILE='${package}.tar.bz2'
+    URL='$FTP_URL_PREFIX/$FILE_PREFIX-${platform}-'${DOWNLOAD_FILE}
 fi
+
+for platform in 32 64
+do
+    if [ $OS = "Darwin" ]&&[ $platform = 32 ];then
+        echo "strip 32 bit file for Darwin"
+        continue
+    fi
+    mkdir -p mkl/x86_${platform}
+    for package in "mkl-include" "mkl-static"
+    do
+        echo "Installing $(eval echo $DOWNLOAD_FILE) for x86_${platform}..."
+        echo "try download mkl package from: $(eval echo $URL)"
+        wget -q --show-progress "$(eval echo $URL)" -O mkl/x86_${platform}/"$(eval echo $DOWNLOAD_FILE)"
+        $TAR xvj -C mkl/x86_${platform} -f mkl/x86_${platform}/"$(eval echo $DOWNLOAD_FILE)"
+    done
+done
