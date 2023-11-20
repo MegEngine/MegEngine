2ebe7fa1f5b2adc3a99fb413c4503e1b4c0146a7
Megvii Engine Team <megengine@megvii.com>
2023-11-20 13:54:13 +0800

    fix(dnn/cambricon): fix compilation error of checksum op when the mlu-arch is mtp_592
    GitOrigin-RevId: fea2e5c2f55f71b4cdbb1b2bcf98786130d2e979

--- dnn/src/cambricon/checksum/checksum.mlu.h
+++ dnn/src/cambricon/checksum/checksum.mlu.h
@@ -2,20 +2,9 @@
 
 #include "src/cambricon/utils.mlu.h"
 
-#if CNRT_MAJOR_VERSION >= 5
 void checksum_kernel_union1_wrapper(
         uint32_t* dst, const uint32_t* src, int num_elems, cnrtQueue_t queue);
 void checksum_kernel_union4_wrapper(
         uint32_t* dst, const uint32_t* src, int num_elems, cnrtQueue_t queue);
-#else
-#ifdef __cplusplus
-extern "C" {
-#endif
-void checksum_kernel_union1(uint32_t* dst, const uint32_t* src, int num_elems);
-void checksum_kernel_union4(uint32_t* dst, const uint32_t* src, int num_elems);
-#ifdef __cplusplus
-}
-#endif
-#endif
 
 // vim: ft=cpp syntax=cpp.doxygen
--- dnn/src/cambricon/checksum/opr_impl.cpp
+++ dnn/src/cambricon/checksum/opr_impl.cpp
@@ -9,7 +9,6 @@ using namespace megdnn;
 using namespace cambricon;
 
 namespace {
-#if CNRT_MAJOR_VERSION >= 5
 void bang_c_wrapper(
         uint32_t* dst, const uint32_t* src, int nr_elems, cnrtQueue_t queue,
         int core_version) {
@@ -20,36 +19,6 @@ void bang_c_wrapper(
     }
     after_kernel_launch();
 }
-#else
-void bang_c_wrapper(
-        uint32_t* dst, const uint32_t* src, int nr_elems, cnrtQueue_t queue,
-        int core_version) {
-    cnrtKernelParamsBuffer_t params;
-    cnrt_check(cnrtGetKernelParamsBuffer(&params));
-    cnrt_check(cnrtKernelParamsBufferAddParam(params, &dst, sizeof(uint32_t*)));
-    cnrt_check(cnrtKernelParamsBufferAddParam(params, &src, sizeof(uint32_t*)));
-    cnrt_check(cnrtKernelParamsBufferAddParam(params, &nr_elems, sizeof(int)));
-    if (core_version == 270) {
-        cnrtDim3_t dim;
-        dim.x = 16;
-        dim.y = 1;
-        dim.z = 1;
-        cnrtFunctionType_t c = CNRT_FUNC_TYPE_UNION4;
-        cnrt_check(cnrtInvokeKernel_V2(
-                (void*)&checksum_kernel_union4, dim, params, c, queue));
-    } else if (core_version == 220) {
-        cnrtDim3_t dim;
-        dim.x = 4;
-        dim.y = 1;
-        dim.z = 1;
-        cnrtFunctionType_t c = CNRT_FUNC_TYPE_UNION1;
-        cnrt_check(cnrtInvokeKernel_V2(
-                (void*)&checksum_kernel_union1, dim, params, c, queue));
-    }
-    after_kernel_launch();
-    cnrt_check(cnrtDestroyKernelParamsBuffer(params));
-}
-#endif
 }  // namespace
 
 size_t ChecksumForwardImpl::get_workspace_in_bytes(const TensorLayout& /* data */) {
