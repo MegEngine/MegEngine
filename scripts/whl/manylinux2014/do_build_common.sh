8cff9db3750f0ff14a8ddd54f37db35af192ca83
Megvii Engine Team <megengine@megvii.com>
2023-11-27 17:27:52 +0800

    build(do_build_common_sh): fix the missing _mgb.so
    GitOrigin-RevId: 6572e1a4aa1f598beef4404bef2e6154889788b5

--- scripts/whl/manylinux2014/do_build_common.sh
+++ scripts/whl/manylinux2014/do_build_common.sh
@@ -78,15 +78,14 @@ function patch_elf_depend_lib_mgb_mge() {
     patchelf --remove-rpath ${BUILD_DIR}/staging/megengine/core/_imperative_rt.so
     patchelf --force-rpath --set-rpath '$ORIGIN/lib' ${BUILD_DIR}/staging/megengine/core/_imperative_rt.so
     handle_strip ${BUILD_DIR}/staging/megengine/core/_imperative_rt.so
+    patch_elf_depend_lib_name ${BUILD_DIR}/staging/megengine/core/_imperative_rt.so
 
     cp ${BUILD_DIR}/src/libmegengine_shared.so ${LIBS_DIR}
     handle_strip ${LIBS_DIR}/libmegengine_shared.so
     patchelf --remove-rpath ${LIBS_DIR}/libmegengine_shared.so
     patchelf --force-rpath --set-rpath '$ORIGIN/.' ${LIBS_DIR}/libmegengine_shared.so
-
-    patch_elf_depend_lib_name ${BUILD_DIR}/staging/megbrain/_mgb.so
     patch_elf_depend_lib_name ${LIBS_DIR}/libmegengine_shared.so
-    patch_elf_depend_lib_name ${BUILD_DIR}/staging/megengine/core/_imperative_rt.so
+
     # as some version of cudnn/trt libs have dlopen libs, so we can not use auditwheel
     # TODO: PR for auditwheel to support args for dlopen libs
     handle_copy_libs ${LIBS_DIR}
